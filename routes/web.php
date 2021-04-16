205e53f9a933e9a64bcdebcb8f7f61af1da8e7d6
zakariaMogh <zakaria.dehaba@gmail.com>
2021-04-16 13:48:55 +0100

    removed conf routes

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('', [\App\Http\Controllers\Admin\AuthController::class, 'create']);
 
-
-Route::get('artisan', function (){
-    \Illuminate\Support\Facades\Artisan::call("migrate:fresh --seed");
-    return "done";
-});
-
-Route::get('migrate', function (){
-    \Illuminate\Support\Facades\Artisan::call("migrate");
-    return "done";
-});
-
-Route::get('install_passport', function (){
-//    //\Illuminate\Support\Facades\Artisan::call("migrate");
-    \Illuminate\Support\Facades\Artisan::call("passport:install");
-    \Illuminate\Support\Facades\Artisan::call("passport:keys");
-    \Illuminate\Support\Facades\Artisan::call("passport:client --personal", ['--force' => true]);
-    \Illuminate\Support\Facades\Artisan::call("config:clear");
-    return "done";
-});
-
-Route::get('storage', function (){
-    \Illuminate\Support\Facades\Artisan::call("storage:link");
-    return "done";
-});
+//
+//Route::get('artisan', function (){
+//    \Illuminate\Support\Facades\Artisan::call("migrate:fresh --seed");
+//    return "done";
+//});
+//
+//Route::get('migrate', function (){
+//    \Illuminate\Support\Facades\Artisan::call("migrate");
+//    return "done";
+//});
+//
+//Route::get('install_passport', function (){
+////    //\Illuminate\Support\Facades\Artisan::call("migrate");
+//    \Illuminate\Support\Facades\Artisan::call("passport:install");
+//    \Illuminate\Support\Facades\Artisan::call("passport:keys");
+//    \Illuminate\Support\Facades\Artisan::call("passport:client --personal", ['--force' => true]);
+//    \Illuminate\Support\Facades\Artisan::call("config:clear");
+//    return "done";
+//});
+//
+//Route::get('storage', function (){
+//    \Illuminate\Support\Facades\Artisan::call("storage:link");
+//    return "done";
+//});
 
 //Route::get('artisan', function (){
 //    \Illuminate\Support\Facades\Artisan::call("migrate");
